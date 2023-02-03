a7e9d2b3a7393415e54d7b327f1ab2c57bb11a5d
FelixIwer <49561587+FelixIwer@users.noreply.github.com>
2023-02-03 04:23:53 +0100

    ProjectUpload

---
+++ ClockJavaScript/dateManagement.js
@@ -0,0 +1,68 @@
+window.onload = function(){
+
+var elHourOutput = document.getElementById("hourOutput");
+var elMinOutput = document.getElementById("minOutput");
+var elSecOutput = document.getElementById("secOutput");
+var elDateOutput = document.getElementById("dateOutput");
+
+var startTime = new Date();
+
+elDateOutput.innerText = startTime.getDate() + "" + startTime.getMonth() + "  " + startTime.getFullYear();
+
+if(startTime.getHours() < 10) {
+    elHourOutput.innerText = "0" + startTime.getHours();
+}else {
+    elHourOutput.innerText = startTime.getHours();
+}
+
+if(startTime.getMinutes() < 10){
+    elMinOutput.innerText = "0" + startTime.getMinutes();
+}else {
+    elMinOutput.innerText = startTime.getMinutes();
+}
+
+if(startTime.getSeconds()< 10){
+    elSecOutput.innerText = "0" + startTime.getSeconds();
+}else{
+    elSecOutput.innerText = startTime.getSeconds()
+}
+
+startClock();
+
+//Functions
+
+function timeCounter(){
+    var newTime = new Date();
+    console.log(newTime.getSeconds());
+    if (newTime.getSeconds() < 10){
+        elSecOutput.innerText = "0" + newTime.getSeconds();
+    }else{
+        elSecOutput.innerText = newTime.getSeconds();
+    }
+
+    if (newTime.getSeconds() == 0){
+        if (newTime.getMinutes() < 10){
+            elMinOutput.innerText = "0" + newTime.getMinutes();
+        }else{
+            elMinOutput.innerText = newTime.getMinutes();
+        }
+    }
+
+    if (newTime.getMinutes() == 0){
+        if (newTime.getHours() < 10){
+            elHourOutput.innerText = "0"  + newTime.getHours();
+        } else {
+            elHourOutput.innerText = newTime.getHours();
+        }
+    }
+
+    if (newTime.getHours() == 0){
+        elDateOutput.innerText = newTime.getDate() + "" + newTime.getMonth() + "  " + newTime.getFullYear();
+    }
+}
+
+function startClock(){
+    setInterval(timeCounter, 1000);
+}
+
+}
---
+++ StopWatchJavaScript/Clockmanagement.js
@@ -0,0 +1,87 @@
+window.onload = function() {
+
+// Document Elements
+var elMinuteCounter = document.getElementById('minuteCounter');
+var elSecondCounter = document.getElementById('secondCounter');
+var elMSCounter = document.getElementById('msCounter');
+var elStartButton = document.getElementById('startButton');
+var elStopButton = document.getElementById('stopButton');
+var elResetButton = document.getElementById('resetButton');
+
+elStartButton.addEventListener("click", startTimer);
+elStopButton.addEventListener("click", stopTimer);
+elResetButton.addEventListener("click", resetTimer);
+
+var timeMin = 0;
+var timeSec = 0;
+var timeMS = 0;
+var workingClock;
+
+function timeCounter (){
+    if(timeMS == 99){
+        timeSec += 1;
+        if(timeSec < 10){
+            elSecondCounter.innerText = "0" + timeSec;
+        } else{
+            elSecondCounter.innerText = timeSec;
+        }
+        timeMS = 0;
+    }
+
+    if(timeSec == 60){
+        timeMin += 1;
+        timeSec = 0;
+        elSecondCounter.innerText = 0;
+        if(timeMin < 10){
+            elMinuteCounter.innerText = "0" + timeMin;
+        } else{
+            elMinuteCounter.innerText = timeMin;
+        }
+    }
+    
+    if(timeMin == 60){
+        stopTimer();
+    }
+
+    timeMS+=1;
+
+    if(timeMS < 10){
+        elMSCounter.innerText = "0" + timeMS;
+    } else{
+        elMSCounter.innerText = timeMS;
+    }
+}
+
+function startTimer() {
+    // setInterval in ms umgerechnet, heißt eine 1 sec = 1000 ms
+    workingClock = setInterval(timeCounter, 10);
+
+    elStartButton.style.visibility = "hidden";
+    elStopButton.style.visibility = "visible";
+    elResetButton.style.visibility = "visible";
+}
+
+function stopTimer() {
+    clearInterval(workingClock);
+
+    elStartButton.style.visibility = "visible";
+    elStopButton.style.visibility = "hidden";
+}
+
+function resetTimer(){
+    clearInterval(workingClock);
+    
+    timeMin = 0;
+    timeSec = 0;
+    timeMS = 0;
+
+    elMSCounter.innerText = "00";
+    elSecondCounter.innerText = "00";
+    elMinuteCounter.innerText = "00";
+
+    elStartButton.style.visibility = "visible";
+    elStopButton.style.visibility = "hidden";
+    elResetButton.style.visibility = "hidden";
+}
+
+}
