9282b617be7bf8b1e2b057e64c48a20a5cf244a3
FelixIwer <49561587+FelixIwer@users.noreply.github.com>
2023-02-22 22:48:33 +0100

    Bugfixes + Tailwind in Clock

--- ClockJavaScript/dateManagement.js
+++ ClockJavaScript/dateManagement.js
@@ -6,8 +6,13 @@ var elSecOutput = document.getElementById("secOutput");
 var elDateOutput = document.getElementById("dateOutput");
 
 var startTime = new Date();
+var rightMonth = startTime.getMonth() + 1;
 
-elDateOutput.innerText = startTime.getDate() + " ." + startTime.getMonth() + " . " + startTime.getFullYear();
+if (rightMonth < 10){
+    elDateOutput.innerText = startTime.getDate() + ".0" + rightMonth + "." + startTime.getFullYear();
+} else {
+    elDateOutput.innerText = startTime.getDate() + "." + rightMonth + "." + startTime.getFullYear();
+}
 
 if(startTime.getHours() < 10) {
     elHourOutput.innerText = "0" + startTime.getHours();
@@ -21,7 +26,7 @@ if(startTime.getMinutes() < 10){
     elMinOutput.innerText = startTime.getMinutes();
 }
 
-if(startTime.getSeconds()< 10){
+if(startTime.getSeconds() < 10){
     elSecOutput.innerText = "0" + startTime.getSeconds();
 }else{
     elSecOutput.innerText = startTime.getSeconds()
@@ -33,7 +38,8 @@ startClock();
 
 function timeCounter(){
     var newTime = new Date();
-    console.log(newTime.getSeconds());
+    var rightMonthNewTime = newTime.getMonth() + 1;
+
     if (newTime.getSeconds() < 10){
         elSecOutput.innerText = "0" + newTime.getSeconds();
     }else{
@@ -57,7 +63,7 @@ function timeCounter(){
     }
 
     if (newTime.getHours() == 0){
-        elDateOutput.innerText = newTime.getDate() + "" + newTime.getMonth() + "  " + newTime.getFullYear();
+        elDateOutput.innerText = newTime.getDate() + "" + rightMonthNewTime + "  " + newTime.getFullYear();
     }
 }
 
