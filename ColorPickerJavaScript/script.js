2de432d1362e2978cd64b2faac3d2e89a59bd460
FelixIwer <49561587+FelixIwer@users.noreply.github.com>
2023-02-23 19:06:32 +0100

    ColorPicker - New Save Button + Small Fix

--- ColorPickerJavaScript/script.js
+++ ColorPickerJavaScript/script.js
@@ -1,19 +1,24 @@
 window.onload = function() {
     const elColorChangeButton = document.getElementById("btnColorChange");
     const elColorResetButton = document.getElementById("btnColorReset");
+    const elLastColorButton = document.getElementById("btnLastColor");
     const elColorNameP = document.getElementById("colorName");
 
     elColorChangeButton.addEventListener("click", colorChanger);
     elColorResetButton.addEventListener("click", standardColor);
+    elLastColorButton.addEventListener("click", getLastColor);
+
+    var oldRandomColor = "rgb(255,255,255)";
 
     function colorChanger(){
+        oldRandomColor = document.body.style.backgroundColor;
+        elLastColorButton.style.background = oldRandomColor;
+
         var x = Math.floor(Math.random() * 256);
         var y = Math.floor(Math.random() * 256);
         var z = Math.floor(Math.random() * 256);
 
         var randomColor = "rgb("+ x + "," + y + "," + z +")";
-
-        console.log(randomColor);
 
         document.body.style.backgroundColor = randomColor;
         elColorNameP.textContent = "RGB ("+ x + "," + y + "," + z +")";
@@ -21,6 +26,13 @@ window.onload = function() {
 
     function standardColor(){
         document.body.style.backgroundColor = "white";
-        elColorNameP.textContent = "";
+        elColorNameP.textContent = "RGB (255,255,255)";
+        oldRandomColor = "rgb(255,255,255)";
+        elLastColorButton.style.background = "white";
+    }
+
+    function getLastColor(){
+        elLastColorButton.style.background = oldRandomColor;
+        document.body.style.backgroundColor = oldRandomColor;
     }
 }
