a45adc0813070d1e385e8c2471ba8b223849e40f
FelixIwer <49561587+FelixIwer@users.noreply.github.com>
2023-03-01 03:26:53 +0100

    New Features for ColorPicker

--- ColorPickerJavaScript/script.js
+++ ColorPickerJavaScript/script.js
@@ -1,21 +1,22 @@
 window.onload = function() {
 
-    // Declaration
+    // DOM Declaration
     const elColorChangeButton = document.getElementById("btnColorChange");
     const elColorResetButton = document.getElementById("btnColorReset");
     const elLastColorButton = document.getElementById("btnLastColor");
+    const elSaveFavoButton = document.getElementById("btnSaveFavo");
+    const elBackToFavoButton = document.getElementById("btnBackToFavo");
+    const elFaveColorName = document.getElementById("favColorName");
     const elColorNameP = document.getElementById("colorName");
 
-    elColorChangeButton.addEventListener("click", colorChanger);
-    elColorResetButton.addEventListener("click", standardColor);
-    elLastColorButton.addEventListener("click", getLastColor);
-
-    var oldRandomColor = "rgb(255,255,255)";
+    // Variable Declaration
+    var oldColor = "rgb(255,255,255)";
+    var favColor = "rgb(255,255,255)";
 
     // Function for Random Color Button
-    function colorChanger(){
-        oldRandomColor = document.body.style.backgroundColor;
-        elLastColorButton.style.background = oldRandomColor;
+    elColorChangeButton.addEventListener("click", function () {
+        oldColor = document.body.style.backgroundColor;
+        elLastColorButton.style.background = oldColor;
 
         var x = Math.floor(Math.random() * 256);
         var y = Math.floor(Math.random() * 256);
@@ -25,19 +26,45 @@ window.onload = function() {
 
         document.body.style.backgroundColor = randomColor;
         elColorNameP.textContent = "RGB ("+ x + "," + y + "," + z +")";
-    }
+    });
 
     // Reset Function
-    function standardColor(){
-        document.body.style.backgroundColor = "white";
+    elColorResetButton.addEventListener("click", function () {
+        document.body.style.backgroundColor = "rgb(255,255,255)";
         elColorNameP.textContent = "RGB (255,255,255)";
-        oldRandomColor = "rgb(255,255,255)";
+        oldColor = "rgb(255,255,255)";
         elLastColorButton.style.background = "white";
-    }
+    });
 
     // Save Last Color Function
-    function getLastColor(){
-        elLastColorButton.style.background = oldRandomColor;
-        document.body.style.backgroundColor = oldRandomColor;
-    }
+    elLastColorButton.addEventListener("click", function () {
+        elLastColorButton.style.background = oldColor;
+        var numbersStr = oldColor.substring(3);
+        var whiteSpaceLess = numbersStr.replace(/\s/g, '');
+        var editText = "RGB " + whiteSpaceLess;
+        elColorNameP.textContent = editText;
+        document.body.style.backgroundColor = oldColor;
+    });
+
+    // Save Favorite Color of User (No Database involved)
+    elSaveFavoButton.addEventListener("click", function () {
+        console.log(document.body.style.backgroundColor);
+        if (document.body.style.backgroundColor != ""){
+            favColor = document.body.style.backgroundColor;
+            var numbersStr2 = favColor.substring(3);
+            var whiteSpaceLess2 = numbersStr2.replace(/\s/g, '');
+            var editText2 = "RGB " + whiteSpaceLess2;
+            elFaveColorName.textContent = editText2;
+            elFaveColorName.style.visibility = "visible";
+            elBackToFavoButton.style.visibility = "visible";
+        }
+    });
+
+    elBackToFavoButton.addEventListener("click", function () {
+        document.body.style.backgroundColor = favColor;
+        var numbersStr3 = favColor.substring(3);
+        var whiteSpaceLess3 = numbersStr3.replace(/\s/g, '');
+        var editText3 = "RGB " + whiteSpaceLess3;
+        elColorNameP.textContent = editText3;
+    });
 }
